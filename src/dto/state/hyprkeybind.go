02bed407ea0e827529af3369faff5373f4db7913
Luuk Blankenstijn <luukblankenstijn@gmail.com>
2025-06-03 08:42:44 +0200

    fix: hyprkeybinds using wrong label for Modmask

--- src/dto/state/hyprkeybind.go
+++ src/dto/state/hyprkeybind.go
@@ -1,7 +1,7 @@
 package state
 
 type HyprKeybind struct {
-	Mod        int    `json:"mod"`
+	Mod        int    `json:"modmask"`
 	Key        string `json:"key"`
 	Dispatcher string `json:"dispatcher"`
 	Arg        string `json:"arg"`
